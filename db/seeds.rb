67abc1d307b74f16b37032464c44447cfd006be2
Randy Rodriguez <Randyr2125@msn.com>
2021-05-04 19:44:53 -0400

    Added a product to seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# rails generate model Products name:string price:integer img_url:string description:string
+product = Products.create(name: "Sony Playstation 5", price:599, img_url: "example.jpg", description: "A gaming system, that you cant find")
