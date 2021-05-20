590bf3b216280f00e41f98005d7aacdf778b30f6
Randy Rodriguez <Randyr2125@msn.com>
2021-05-19 22:36:45 -0400

    added has_many :category through: :.. code

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   # authenticate user:
-  before_action :authenticate_admin except: [:index, :show]
+  before_action :authenticate_admin, except: [:index, :show]
   
   def index
   orders = Order.all
--- app/models/product.rb
+++ app/models/product.rb
@@ -14,6 +14,8 @@ class Product < ApplicationRecord
 belongs_to :supplier
 
 has_many :images
+has_many :categories
+has_many :categories, through: :category_products 
 
 def is_discounted
   if price < 10
